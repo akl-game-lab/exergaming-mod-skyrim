37b3ebac6c94b4c21865a46d8a8a8351d014610c
Mitchell1994 <mitchell.longair1@gmail.com>
2016-07-05 11:50:53 +1200

    Changed webserviceTest to use JSON

--- webserviceTest/webserviceTest/webserviceTest.cpp
+++ webserviceTest/webserviceTest/webserviceTest.cpp
@@ -18,7 +18,11 @@ using namespace concurrency::streams;       // Asynchronous streams
 std::string NORMAL_FETCH = "0";
 std::string FORCE_FETCH = "1";
 
+std::string URL_BASE = "http://ec2-54-252-163-152.ap-southeast-2.compute.amazonaws.com:3001/users/";
+
+/*
 std::string URL_BASE = "http://ec2-54-252-163-152.ap-southeast-2.compute.amazonaws.com:3000/users/";
+*/
 
 /*======================
 	Helper functions
@@ -33,61 +37,6 @@ std::string toString(const std::wstring &wstr)
 	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
 	return strTo;
 }
-
-/*======================
-	Main functions
-======================*/
-
-/*
-int main(int argc, char* argv[])
-{
-	std::string gameID = argv[0];
-	std::string userName = argv[1];
-	std::string fromDate = argv[2];
-	std::string toDate = argv[3];
-
-	std::string url = "http://exergaming.isaac.techrus.co.nz/poc_api/get-workout/" + gameID + "/" + userName + "/" + fromDate + "/" + toDate;
-	uri fullUri(conversions::to_string_t(url));
-
-	auto fileStream = std::make_shared<ostream>();
-
-	// Open stream to output file.
-	pplx::task<void> requestTask = fstream::open_ostream(U("Raw_Data.xml")).then([=](ostream outFile)
-	{
-		*fileStream = outFile;
-
-		// Create http_client to send the request.
-		http_client client(fullUri);
-
-		// Build request URI and start the request.
-		uri_builder builder(U("/search"));
-		builder.append_query(U("q"), U("Casablanca CodePlex"));
-		return client.request(methods::GET, builder.to_string());
-	})
-
-		// Handle response headers arriving.
-		.then([=](http_response response)
-	{
-		// Write response body into the file.
-		return response.body().read_to_end(fileStream->streambuf());
-	})
-
-		// Close the file stream.
-		.then([=](size_t)
-	{
-		return fileStream->close();
-	});
-
-	// Wait for all the outstanding I/O to complete and handle any exceptions
-	try
-	{
-		requestTask.wait();
-	}
-	catch (const std::exception &e)
-	{
-	}
-	return 0;
-}*/
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -109,7 +58,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine,
 	if (type == "NORMAL"){
 		std::string fromDate = toString(szArgList[3]);
 		std::string toDate = toString(szArgList[4]);
-		url = URL_BASE + username + "/workouts/" + fromDate + "/" + toDate;
+		url = URL_BASE + username + "/workouts/hsm/" + fromDate + "/" + toDate;
 	}
 
 	LocalFree(szArgList);
